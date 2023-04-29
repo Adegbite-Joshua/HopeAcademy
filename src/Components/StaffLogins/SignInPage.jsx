f9a24f62f6f7667fddac26198cc1397b072d6f77
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-04-29 18:31:02 +0100

    Working on the staff login page

--- src/Components/StaffLogins/SignInPage.jsx
+++ src/Components/StaffLogins/SignInPage.jsx
@@ -6,11 +6,11 @@ const SignInPage = () => {
         <div className='w-full h-24 h-screen bg-purple-600'>
             <div className="w-6/12 h-auto block mx-auto">
                 <label htmlFor="">Email</label>
-                <input type="text" className='w-full form focus:outline-none focus:ring focus:ring-2 focus:ring-red rounded-lg placeholder-text-slate-500' placeholder='Email' name='' />
+                <input type="text" className='w-full form focus:outline-none focus:ring focus:ring-2 focus:ring-red rounded-md placeholder-text-slate-500' placeholder='Email' name='' />
                 <label htmlFor="">Id Number</label>
-                <input type="text" className='w-full form focus:outline-none focus:ring focus:ring-2 focus:ring-red rounded-lg placeholder-text-slate-500' placeholder='Staff Id' name='' />
+                <input type="text" className='w-full form focus:outline-none focus:ring focus:ring-2 focus:ring-red rounded-md placeholder-text-slate-500' placeholder='Staff Id' name='' />
                 <label htmlFor="">Password</label>
-                <input type="text" className='w-full form focus:outline-none focus:ring focus:ring-2 focus:ring-red rounded-lg placeholder-text-slate-500' placeholder='Staff Password' name='' />
+                <input type="text" className='w-full form focus:outline-none focus:ring focus:ring-2 focus:ring-red rounded-md placeholder-text-slate-500' placeholder='Staff Password' name='' />
 
             </div>
 
