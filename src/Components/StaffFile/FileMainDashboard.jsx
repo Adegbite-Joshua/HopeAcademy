2428fbe3608c2efdd28cc619f931655b0c39c4fb
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-05-04 22:26:49 +0100

    Workingo the post file page and message after

--- src/Components/StaffFile/FileMainDashboard.jsx
+++ src/Components/StaffFile/FileMainDashboard.jsx
@@ -6,9 +6,30 @@ const FileMainDashboard = () => {
     <>
         <div className="FileMainDashboard middleDiv">
             {/* <FileView/> */}
-            <object data="vite.svg" width="100%" height="100%"> {/* type="application/pdf" can also be added */} 
-                <p>Alternative text - include a link <a href="port.pdf">to the PDF!</a></p>
-            </object>
+            {/* type="application/pdf" can also be added */} 
+            
+            <div>
+
+              <form action="" method="post">
+                <label htmlFor="fileTitle">File Title</label>
+                <input type="text" className=' my-1 w-full rounded-md p-2 focus:outline-0 focus:ring focus:ring-2 focus:ring-violet-500' placeholder='File Title' />
+                <label htmlFor="">File's Description</label>
+                <textarea name="" id="" className=' my-1 w-full h-24 focus:outline-0 focus:ring focus:ring-2 focus:ring-violet-500 p-2' placeholder='Post Describtion'></textarea>
+                <div className=' w-full md:w-3/6 aspect-square block mx-auto'>
+                    <object data="sound.p3" width="100%" height="100%">  
+                        <div className=' bg-black flex w-full h-full items-center justify-center'>
+                            <p className=' text-white'>The Choosed File Wil Appear Here</p>
+                        </div>
+                    </object>
+                </div>
+                <label htmlFor="" className='w-full'>
+                    <span className="sr-only">Choose Fil To Upload</span>
+                    <input type="file" className=' w-full my-1 block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:rounded-full file:border-0 file:text-sm file:font-bold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100' />
+                </label>
+                <button type='button' className=' w-3/6 block mx-auto p-2 my-1 bg-blue-600 hover:bg-blue-500 rounded-md'>Send File</button>
+              </form>
+              
+            </div>
         </div>
     </>
   )
--- src/Components/StaffMessage/SenderMessages.jsx
+++ src/Components/StaffMessage/SenderMessages.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 const SenderMessages = () => {
   return (
     <>
-        <div className=' w-64 md:w-96 h-auto bg-slate-300 block ms-auto m-1 p-2 rounded-md'>
-            <img src="vite.svg" className='h-12 w-12 float-left' alt="" />
-            <p>Hello. How are you today?</p>
-            <p className='text-right'><small>10:00 PM</small></p>
+        <div className=' w-64 md:w-96 h-auto bg-slate-300 block ms-auto   relative'>
+            <div className=' m-1 p-2 rounded-md block absolute inset-0 '></div>
+            <div className=' m-1 p-2 rounded-md relative'>
+              <img src="vite.svg" className='h-12 w-12 float-left' alt="" />
+              <p>Hello. How are you today?</p>
+              <p className='text-right'><small>10:00 PM</small></p>
+            </div>
         </div>
     </>
   )
