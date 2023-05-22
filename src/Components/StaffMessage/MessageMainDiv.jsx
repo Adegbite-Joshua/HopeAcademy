8bbc337464d049675d5b2319d19f5bdcbe37dd0c
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-05-22 22:15:10 +0100

    Working on messafing others

--- src/Components/StaffMessage/MessageMainDiv.jsx
+++ src/Components/StaffMessage/MessageMainDiv.jsx
@@ -13,26 +13,31 @@ const MessageMainDiv = ({mainindexRec, categoryRec, individualIndexRec}) => {
   return (
     <>
         <div className='MessageMainDiv mt-16 md:mt-0 h-screen basis-full md:basis-7/12 px-5 overflow-y-auto'>
-            <h3 className=' text-center font-bold'>Adegbite Joshua</h3>
-            <div className='w-full bg-white overflow-y-auto p-2'>
-                {/* <SenderMessages/>
-                <ReceiverMessages/>
-                <SenderMessages/>
-                <SenderMessages/>
-                <ReceiverMessages/>
-                <SenderMessages/>
-                <SenderMessages/>
-                <ReceiverMessages/>
-                <SenderMessages/>
-                <SenderMessages/>
-                <ReceiverMessages/>
-                <SenderMessages/>
-                <SenderMessages/>
-                <ReceiverMessages/>
-                <SenderMessages/> */}
-                {/* {category==0?allStudents[mainindex][index]} */}
-                <div className=' w-full h-80 m-auto flex justify-center items-center'>
-                  <h2 className=' p-2 bg-blue-500 rounded-md'>Select A Name To Chat With </h2>
+            <div className='w-full h-full bg-white overflow-y-auto p-2'>
+                <div className=' h-3/6 overflow-y-auto'>
+                  {/* <SenderMessages/>
+                  <ReceiverMessages/>
+                  <SenderMessages/>
+                  <SenderMessages/>
+                  <ReceiverMessages/>
+                  <SenderMessages/>
+                  <SenderMessages/>
+                  <ReceiverMessages/>
+                  <SenderMessages/>
+                  <SenderMessages/>
+                  <ReceiverMessages/>
+                  <SenderMessages/>
+                  <SenderMessages/>
+                  <ReceiverMessages/>
+                  <SenderMessages/> */}
+                  <div className=' w-full h-80 m-auto flex justify-center items-center'>
+                    <h2 className=' p-2 bg-blue-500 rounded-md'>Select A Name To Chat With </h2>
+                  </div>
+                </div>
+                <div className=' h-3/6 w-full p-2'>
+                  <h3 className=' text-center font-bold'>Adegbite Joshua</h3>
+                  <textarea name="" id="" className=' w-full border border-3 border-red-400' rows="4"></textarea>
+                  <button className=' p-2 rounded-md bg-blue-500 hover:bg-blue-400 block mx-auto'>Send Message</button>
                 </div>
             </div>
         </div>
