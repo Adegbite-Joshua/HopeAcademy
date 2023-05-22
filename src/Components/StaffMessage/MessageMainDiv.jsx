ba678fd96f661ae0959a9e442d8647d7454678e1
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-05-22 22:38:56 +0100

    Working on messaging others

--- src/Components/StaffMessage/MessageMainDiv.jsx
+++ src/Components/StaffMessage/MessageMainDiv.jsx
@@ -10,6 +10,17 @@ const MessageMainDiv = ({mainindexRec, categoryRec, individualIndexRec}) => {
   const [category, setcategory] = useState('');
   const [mainindex, setmainindex] = useState('');
   const [index, setindex] = useState('');
+  const sendMessage = async()=>{
+    let sendMessageEndpoint = 'http://localhost:7777/staff/dashboard'
+    let messageBody = {
+      // messageSender: staffInfo
+    }
+    try {
+      const response = await axios.post(sendMessageEndpoint, messageBody)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <>
         <div className='MessageMainDiv mt-16 md:mt-0 h-screen basis-full md:basis-7/12 px-5 overflow-y-auto'>
@@ -37,7 +48,7 @@ const MessageMainDiv = ({mainindexRec, categoryRec, individualIndexRec}) => {
                 <div className=' h-3/6 w-full p-2'>
                   <h3 className=' text-center font-bold'>Adegbite Joshua</h3>
                   <textarea name="" id="" className=' w-full border border-3 border-red-400' rows="4"></textarea>
-                  <button className=' p-2 rounded-md bg-blue-500 hover:bg-blue-400 block mx-auto'>Send Message</button>
+                  <button onClick={sendMessage} className=' p-2 rounded-md bg-blue-500 hover:bg-blue-400 block mx-auto'>Send Message</button>
                 </div>
             </div>
         </div>
