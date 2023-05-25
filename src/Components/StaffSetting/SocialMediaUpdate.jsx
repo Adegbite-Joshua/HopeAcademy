ced8de4f84023bd327af37decf4171dcaf03cbdf
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-05-25 18:57:26 +0100

    Working on editing the staff informations

--- src/Components/StaffSetting/SocialMediaUpdate.jsx
+++ src/Components/StaffSetting/SocialMediaUpdate.jsx
@@ -9,16 +9,16 @@ const SocialMediaUpdate = () => {
           <h3 classname='p-3'>Add Your Social Media Link</h3>
           <div className='w-100 px-4'>
             <label htmlFor='emailaddress'>Twitter Address</label>
-            <input type='link' defaultValue={staffInfo.links.twitter?staffInfo.links.twitter:''} className='p-2 text-slate-500 rounded-md focus:outline-0 focus:ring focus:ring-2 focus:ring-violet-500 w-full md:w-3/6 block' placeholder='Twitter Address' id='twitteraddress' />
+            <input type='link' defaultValue={staffInfo=={}?staffInfo.links.twitter:''} className='p-2 text-slate-500 rounded-md focus:outline-0 focus:ring focus:ring-2 focus:ring-violet-500 w-full md:w-3/6 block' placeholder='Twitter Address' id='twitteraddress' />
             <div className='block w-full md:w-3/6 ms-auto'>
                 <label htmlFor='facebooklink'>Facebook Link</label>
-                <input type='link' defaultValue={staffInfo.links.facebook?staffInfo.links.facebook:''} className='w-full block p-2 text-slate-500 rounded-md focus:outline-0 focus:ring focus:ring-2 focus:ring-violet-500' placeholder='Facebook Link' id='facebooklink' />
+                <input type='link' defaultValue={staffInfo=={}?staffInfo.links.facebook:''} className='w-full block p-2 text-slate-500 rounded-md focus:outline-0 focus:ring focus:ring-2 focus:ring-violet-500' placeholder='Facebook Link' id='facebooklink' />
             </div>
             <label htmlFor='whatsappNumber'>WhatsApp Link</label>
-            <input type='tel' defaultValue={staffInfo.links.whatsapp?staffInfo.links.whatsapp:''} className='p-2 text-slate-500 rounded-md focus:outline-0 focus:ring focus:ring-2 focus:ring-violet-500 w-full md:w-3/6 block' placeholder='WhatsApp Link' id='whatsapplink' />
+            <input type='tel' defaultValue={staffInfo=={}?staffInfo.links.whatsapp:''} className='p-2 text-slate-500 rounded-md focus:outline-0 focus:ring focus:ring-2 focus:ring-violet-500 w-full md:w-3/6 block' placeholder='WhatsApp Link' id='whatsapplink' />
             <div className='block w-full md:w-3/6 ms-auto'>
                 <label htmlFor='otherLinks'>Other Link</label>
-                <input type='link' defaultValue={staffInfo.links.other?staffInfo.links.other:''} className='w-full block p-2 text-slate-500 rounded-md focus:outline-0 focus:ring focus:ring-2 focus:ring-violet-500' placeholder='Other Link' id='otherlink' />
+                <input type='link' defaultValue={staffInfo=={}?staffInfo.links.other:''} className='w-full block p-2 text-slate-500 rounded-md focus:outline-0 focus:ring focus:ring-2 focus:ring-violet-500' placeholder='Other Link' id='otherlink' />
             </div>
           <button className='w-full bg-blue-600 hover:bg-blue-500 rounded-md p-2 my-2'>Submit</button>
           </div>
--- src/Components/StaffSetting/SubjectSetting.jsx
+++ src/Components/StaffSetting/SubjectSetting.jsx
@@ -4,11 +4,47 @@ import ButtonComp from '../ButtonComp'
 
 const SubjectSetting = () => {
     let staffInfo = useSelector((state)=>state.staffInformation.staffInformation)
+    // const setSubject =()=>{document.getElementById('staffIndex')[staffInfo.staffIndex].selected = 'true'}
+    const subjects = [
+        'MATHEMATICS',
+        'ENGLISH LANGUAGE',
+        'YORUBA',
+        'CIVIC EDUCATION',
+        'COMPUTER STUDIES',
+        'GEOGRAPHY',
+        'ECONOMICS',
+        'PHYSICS',
+        'CHEMISTRY',
+        'BIOLOGY',
+        'ANIMAL HUSBANDRY',
+        'FURTHER MATHEMATICS',
+        'TECHNICAL DRAWING '
+      ]
   return (
     <>
         <div className="SubjectSetting w-full ">
             <label htmlFor="subjectName">Change Subject</label>
-            <input type="text" className='w-full block p-2 text-slate-500 rounded-md focus:outline-0 focus:ring focus:ring-2 focus:ring-violet-500' defaultValue='Mathematics' />
+            {/* <input type="text" className='w-full block p-2 text-slate-500 rounded-md focus:outline-0 focus:ring focus:ring-2 focus:ring-violet-500' defaultValue='Mathematics' /> */}
+            <select  name="staffIndex" id="staffIndex" className='w-full border-slate-900 focus:ring-4 focus:ring-purple focus:outline-none p-2 hover:boder-0 focus:ring-0 rounded-full  placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-50 px-6'>
+                  {/* <option  value='0'>MATHEMATICS</option>
+                  <option  value='1'>ENGLISH LANGUAGE</option>
+                  <option  value='2'>YORUBA</option>
+                  <option  value='3'>CIVIC EDUCATION</option>
+                  <option  value='4'>COMPUTER STUDIES</option>
+                  <option  value='5'>GEOGRAPHY</option>
+                  <option  value='6'>ECONOMICS</option>
+                  <option  value='7'>PHYSICS</option>
+                  <option  value='8'>CHEMISTRY</option>
+                  <option  value='9'>BIOLOGY</option>
+                  <option  value='10'>ANIMAL HUSBANDRY</option>
+                  <option  value='11'>FURTHER MATHEMATICS</option>
+                  <option  value='12 '>TECHNICAL DRAWING</option>
+                   */}
+                   {subjects.map((subject, index) =>(
+                        <option  value={index} selected={index==staffInfo.staffIndex?'true': 'false'}>{subject}</option>
+                   ))}
+                </select>
+                {/* {setSubject} */}
             <label htmlFor="subjectTeacher">Change Subject Information</label>
             <input type="text" className='w-full block p-2 text-slate-500 rounded-md focus:outline-0 focus:ring focus:ring-2 focus:ring-violet-500' defaultValue='' />
             <div className=' w-full md:w-3/6 aspect-square block mx-auto'>
