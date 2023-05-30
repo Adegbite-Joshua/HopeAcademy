647d2c8cbf80b44f2d42451c7b0673237f2d09d1
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-05-30 23:14:06 +0100

    Working on editing the student's assessment

--- src/Components/Student/StudentScoreTable.jsx
+++ src/Components/Student/StudentScoreTable.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
-const StudentScoreTable = () => {
+const StudentScoreTable = ({index}) => {
+    let staffInfo = useSelector((state)=>state.staffInformation.staffInformation)
+    let classStudents = useSelector((state)=>state.staffInformation.classStudents)
+    let subjectIndex = index&&classStudents?classStudents[index].subjects.findIndex({'subject.subject': staffInfo.class}):''
+    const updateStudentAssessment =()=>{
+        
+    }
   return (
     <>
         <table className=' w-full'>
@@ -28,24 +35,24 @@ const StudentScoreTable = () => {
             </thead>
             <tbody id='result'>
             <tr>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
-                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].ca1:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].ca2:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].ca3:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].ca4:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].ca5:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].ass1:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].ass2:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].ass3:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].ass4:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].ass5:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].test1:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].test1:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].test1:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].bonus1:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].bonus2:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].total:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].percent:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
+                <td className=' h-11 w-11 text-center m-1 border border-2 p-1'><input type="number" placeholder='00' defaultValue={subjectIndex?classStudents[index].subjects[subjectIndex].position:'00'} className=' w-full h-full focus:outline-0 focus:ring-2 focus:ring-blue-600 rounded-md text-center' max={5} min={0} name="" id="" /></td>
             </tr>
         </tbody>
         </table>
