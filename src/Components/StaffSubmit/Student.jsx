4e5031e0b67ddaa8a0e6d857d5d3f003324a1b4b
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-06-03 08:43:44 +0100

    Working on selecting a subject to update assessment

--- src/Components/StaffSubmit/Student.jsx
+++ src/Components/StaffSubmit/Student.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Icon from '../Icon'
 
-const Student = () => {
+const Student = ({name}) => {
   return (
     <>
         <div className=' bg-white my-1 rounded-md p-2'>
             <Icon/>
-            <h3>Adegbite Joshua</h3>
+            <h3>{name}</h3>
             <p className='text-right'><small>Active</small></p>
         </div>
     </>
--- src/Components/StaffSubmit/SubmitOtherDiv.jsx
+++ src/Components/StaffSubmit/SubmitOtherDiv.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Student from './Student'
 
 const SubmitOtherDiv = () => {
+  let staffInfo = useSelector((state)=>state.staffInformation.staffInformation)
   return (
     <>
         <div className='SubmitOtherDiv otherDiv'>
             <h3 className=' font-bold text-center'>Choose Student</h3>
             <div className=''>
-                <Student/>
+                {staffInfo.submittedWorks?staffInfo.submittedWorks.map((work, index)=>(
+                  <Student name='Working on it'/>
+                )):<Student name='No Submit Work Yet'/>}
+                <Student name='No Submit Work Yet'/>
             </div>
         </div>
     </>
