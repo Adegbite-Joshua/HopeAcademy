f36c10571e240efb0b5cf78479b4608a09ce8b8d
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-07-01 10:47:06 +0100

    Made changes to the sign in  page

--- src/Components/StaffLogins/SignInPage.jsx
+++ src/Components/StaffLogins/SignInPage.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import { Formik, useFormik } from 'formik'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import * as Yup from 'yup';
 import SnackBar from '../SnackBar';
 
@@ -70,25 +70,30 @@ const SignInPage = () => {
   window.showSnackBar = showSnackBar
   return (
     <>
-        <div className='w-full h-24 h-screen bg-slate-200 pt-24'>
-            <form onSubmit={formik.handleSubmit} className="w-6/12 h-auto block mx-auto">
-                <label htmlFor="" className=''>Email</label>
-                <input onChange={formik.handleChange} type="text" className='w-full border-slate-900 focus:ring-4 focus:ring-purple focus:outline-none p-2 hover:boder-0 focus:ring-0 rounded-full  placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-50' placeholder='Email' name='email' />
-                <label htmlFor="">Class</label>
-                {/* <input onChange={formik.handleChange} type="text" className='w-full border-slate-900 focus:ring-4 focus:ring-purple focus:outline-none p-2 hover:boder-0 focus:ring-0 rounded-full  placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-50' placeholder='Staff Id' name='staffId' /> */}
-                <select name="class" id="class" required onChange={formik.handleChange} className='w-full border-slate-900 focus:ring-4 focus:ring-purple focus:outline-none p-2 hover:boder-0 focus:ring-0 rounded-full  placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-50'>
-                  <option value={0} selected>JSS1</option>
-                  <option value={1}>JSS2</option>
-                  <option value={2}>JSS3</option>
-                  <option value={3}>SSS1</option>
-                  <option value={4}>SSS2</option>
-                  <option value={5}>SSS3</option>
-                </select>
-                <label htmlFor="">Password</label>
-                <input onChange={formik.handleChange} type="text" className='w-full border-slate-900 focus:ring-4 focus:ring-purple focus:outline-none p-2 hover:boder-0 focus:ring-0 rounded-full  placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-50' placeholder='Staff Password' name='password' />
-                <input type="checkbox" className='accent-red-400' name="" id="" /><small>Remenber me</small>
-                <button type='submit' className='block py-2 bg-orange-500 w-full rounded-full hover:bg-orange-300'>Sign In</button>
-            </form>            
+        <div className="flex h-screen">
+          <div className='signinMainDiv bg-slate-200 pt-24 flex justify-center items-center '>
+              <form onSubmit={formik.handleSubmit} className="h-auto mx-auto">
+                  <label htmlFor="" className=''>Email</label>
+                  <input onChange={formik.handleChange} type="text" className='w-full border-slate-900 focus:ring-4 focus:ring-purple focus:outline-none p-2 hover:boder-0 focus:ring-0 rounded-full  placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-50' placeholder='Email' name='email' />
+                  <label htmlFor="">Class</label>
+                  {/* <input onChange={formik.handleChange} type="text" className='w-full border-slate-900 focus:ring-4 focus:ring-purple focus:outline-none p-2 hover:boder-0 focus:ring-0 rounded-full  placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-50' placeholder='Staff Id' name='staffId' /> */}
+                  <select name="class" id="class" required onChange={formik.handleChange} className='w-full border-slate-900 focus:ring-4 focus:ring-purple focus:outline-none p-2 hover:boder-0 focus:ring-0 rounded-full  placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-50'>
+                    <option value={0} selected>JSS1</option>
+                    <option value={1}>JSS2</option>
+                    <option value={2}>JSS3</option>
+                    <option value={3}>SSS1</option>
+                    <option value={4}>SSS2</option>
+                    <option value={5}>SSS3</option>
+                  </select>
+                  <label htmlFor="">Password</label>
+                  <input onChange={formik.handleChange} type="text" className='w-full border-slate-900 focus:ring-4 focus:ring-purple focus:outline-none p-2 hover:boder-0 focus:ring-0 rounded-full  placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-50' placeholder='Staff Password' name='password' />
+                  <input type="checkbox" className='accent-red-400' name="" id="" /><small className='text-white'>Remenber me</small>
+                  <button type='submit' className='block py-2 bg-orange-500 w-full rounded-full hover:bg-orange-300'>Sign In</button>
+              </form>            
+          </div>
+          <div className='signinOtherDiv rounded-4'>
+            <span className='px-3 py-2 rounded-pill bg-blue-700 text-white rounded-full'><Link>Sign Up</Link></span>
+          </div>
         </div>
         <div id='snackbarContainer'><SnackBar body={snacksBarBody} type={snacksBarType}/></div>
     </>
