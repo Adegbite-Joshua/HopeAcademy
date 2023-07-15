689e1fdc4283ff3f35a6121efceca3e42b1f1101
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-07-15 15:48:20 +0100

    Working on creating the staff and joined students video display

--- src/Components/Class/JoinedStudent.jsx
+++ src/Components/Class/JoinedStudent.jsx
@@ -7,9 +7,9 @@ import ClassMainDiv from './ClassMainDiv'
 const JoinedStudent = () => {
   return (
     <>
-       <div className='w-44 h-full relative border-2 border-green-300'>
+       <div className='w-44 h-full relative border-2 border-green-300 shrink-0'>
             <video src='vid.mp4' muted autoPlay controls loop className='w-full h-full'></video>
-            <p className='absolute top-0'>Adegbite Joshua</p>
+            <p className='absolute top-0 bg-white w-full text-center'>Adegbite Joshua</p>
        </div>
     </>
   )
