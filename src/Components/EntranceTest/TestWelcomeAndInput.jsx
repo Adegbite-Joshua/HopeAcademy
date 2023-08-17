bfe919731fb11e4c42f4b1234199674343d03179
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-08-17 11:44:44 +0100

    Working on creating the sign in page and the instruction page to confirm the test commencement

---
+++ src/Components/EntranceTest/TestWelcomeAndInput.jsx
@@ -0,0 +1,76 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+import AlertDialogSlide from './ConfirmationComponent';
+
+
+const TestWelcomeAndInput = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const handleStartTest = () => {
+        if (email.trim() && password.trim()) {
+          axios.post('http://localhost:7777/student/entrancetestsignin', {email, password})
+          .then((res)=>{
+            if (res.status==200) {
+                sessionStorage.setItem('entrance_test_login', JSON.stringify({email,password}))
+                window.location.href += 'instructions'
+            }
+          })
+          .catch((error)=>{
+            console.log(error);
+          })
+        } else {
+          alert('Please enter both email and password.');
+        }
+    };
+
+  return (
+    <>
+          <div className="max-w-xl mx-auto p-6">
+              <h1 className="text-3xl font-semibold mb-4 text-center">Welcome to Our School's Test!</h1>
+              <p className="mb-4 text-center">
+                  We are excited to have you take our school's test. This test is an important part of our admission process.
+                  Please enter your email and password to begin the test.
+              </p>
+              <div className="mb-4">
+                  <label htmlFor="email" className="block font-medium mb-2">
+                      Email:
+                  </label>
+                  <input
+                      type="email"
+                      id="email"
+                      className="w-full p-2 border rounded"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                  />
+              </div>
+              <div className="mb-4">
+                  <label htmlFor="password" className="block font-medium mb-2">
+                      Password:
+                  </label>
+                  <input
+                      type="password"
+                      id="password"
+                      className="w-full p-2 border rounded"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                  />
+              </div>
+              <button
+                  className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded"
+                  onClick={handleStartTest}
+              >
+                  Start Test
+              </button>
+              {/* <AlertDialogSlide text={<>
+                <p className="mb-2">You are about to start a test with 20 questions to be completed in 15 minutes.</p>
+                <p className="mb-4">Are you ready to begin?</p>
+          </>} /> */}
+          </div>
+    </>
+  )
+}
+
+export default TestWelcomeAndInput
