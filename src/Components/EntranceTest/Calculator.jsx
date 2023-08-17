0ae38cb988e2c377c7eb90318b6fbd7898df84ee
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-08-17 22:31:04 +0100

    Added more functions the calculator

--- src/Components/EntranceTest/Calculator.jsx
+++ src/Components/EntranceTest/Calculator.jsx
@@ -27,13 +27,31 @@ export default function AlertDialogSlide({showDialog, setDialog, text}) {
     }
   };
 
+  const handleDelete = () => {
+    setInput((prevInput) => prevInput.slice(0, -1));
+    calc_input.focus();
+  };
+
   const handleClear = () => {
     setInput('');
   };
 
   const handleButtonClick = (value) => {
     setInput((prevInput) => prevInput + value);
+    calc_input.focus();
   };
+
+  const handleInputChange = (event) => {
+    const newInput = event.target.value.replace(/[^0-9+\-*/.]/g, '');
+    setInput(newInput);
+  };
+
+
+  const validateInput =(input)=> {
+    input.value = input.value.replace(/[^0-9+\-*/.]/g, '');
+    setInput((prevInput) => prevInput + input.value);
+    // console.log(input);
+  }
 
   useEffect(() => {
     setOpen(showDialog)
@@ -51,12 +69,13 @@ export default function AlertDialogSlide({showDialog, setDialog, text}) {
         <DialogTitle>{"Calculator"}</DialogTitle>
         <DialogContent>
         <div className="w-72 mt-4 p-4 border rounded shadow-md">
-      <input
-        type="text"
-        value={input}
-        className="w-full p-2 mb-2 text-lg rounded border"
-        readOnly
-      />
+        <input
+          type="text"
+          onChange={handleInputChange}
+          value={input}
+          id="calc_input"
+          className="w-full p-2 mb-2 text-lg rounded border"
+        />
       <div className="grid grid-cols-4 gap-2">
         <button className="p-2 bg-gray-200 hover:bg-gray-300 text-center" onClick={() => handleButtonClick('7')}>7</button>
         <button className="p-2 bg-gray-200 hover:bg-gray-300 text-center" onClick={() => handleButtonClick('8')}>8</button>
@@ -72,7 +91,8 @@ export default function AlertDialogSlide({showDialog, setDialog, text}) {
         <button className="p-2 bg-blue-400 hover:bg-blue-500 text-white text-center" onClick={() => handleButtonClick('*')}>*</button>
         <button className="p-2 bg-gray-200 hover:bg-gray-300 text-center" onClick={() => handleButtonClick('0')}>0</button>
         <button className="p-2 bg-blue-400 hover:bg-blue-500 text-white text-center" onClick={() => handleButtonClick('/')}>/</button>
-        <button className="p-2 bg-blue-400 hover:bg-blue-500 text-white text-center" onClick={handleCalculate}>=</button>
+        <button className="p-2 bg-blue-400 hover:bg-blue-500 text-white text-center" onClick={() => handleButtonClick('/')}>/</button>
+        <button className="p-2 bg-blue-400 hover:bg-blue-500 text-white text-center" onClick={handleDelete}>De</button>
         <button className="p-2 bg-red-400 hover:bg-red-500 text-white text-center col-span-2" onClick={handleClear}>Clear</button>
       </div>
     </div>
