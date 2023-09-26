f7b67c1473eac31ae5c96cd2848bfde5cb2bed0e
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-09-26 14:58:54 +0100

    Working on admin signup page

---
+++ pages/AdminPages/AdminSignUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LandingPageNav from '../../src/Components/LandingPageNav'
+
+const AdminSignUp = () => {
+  return (
+    <>
+        <LandingPageNav/>
+        
+    </>
+  )
+}
+
+export default AdminSignUp
