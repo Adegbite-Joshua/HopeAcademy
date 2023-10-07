32ef25db97386463a312cf31b3b78d07be738a74
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-10-08 00:58:05 +0100

    Created the form validation for new academic term form

--- src/Components/AdminComponents/NewAcademicTerm/NewAcademicTerm.jsx
+++ src/Components/AdminComponents/NewAcademicTerm/NewAcademicTerm.jsx
@@ -1,37 +1,136 @@
 import React from 'react'
 import Navbar from '../NavBar/NavBar'
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios'
+
 
 const NewAcademicTerm = () => {
+    const formik = useFormik({
+        initialValues: {
+          newTerm: '',
+          schoolFeesAmount: '',
+          staffSalary: '',
+          authorizationCode: '',
+          isChecked: false,
+        },
+        validationSchema: Yup.object({
+          newTerm: Yup.string().required('Please Select The New Academic Term'),
+          schoolFeesAmount: Yup.number().typeError('School fees amount must be a number').required('School fees amount is required').positive('School fees amount must be a positive number'),
+          staffSalary: Yup.number().typeError('Staff salary amount must be a number').required('Staff salary amount is required').positive('Staff salary amount must be a positive number'),
+          authorizationCode: Yup.string().required('Please Input Your Admin Authorization Code'),
+          isChecked: Yup.boolean().oneOf([true], 'You must accept the terms and conditions'),
+        }),
+        onSubmit: (values) => {
+          axios.post('http://localhost:7777/admin/sign_up', values)
+          .then((res)=>{
+            console.log(res)
+            if (res.status==200){
+              toast.success('Signup successful!', {
+                position: toast.POSITION.TOP_RIGHT,
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                style: {
+                  background: '#4caf50', // Background color of the toast
+                  color: '#ffffff', // Text color of the toast
+                  fontSize: '16px', // Font size
+                },
+              });
+              setTimeout(() => {navigate('/admin/signin')}, 2000);
+            } 
+          })
+          .catch((error)=>{
+            console.log(error)
+            if (error.response.status==408) {
+              toast.error('Email Already Exist', {
+                position: toast.POSITION.TOP_RIGHT,
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                style: {
+                  background: '#ff5252', 
+                  color: '#ffffff', 
+                  fontSize: '16px',
+                },
+              });
+            } else if (error.response.status==408) {
+              toast.error('Invalid Admin Sign Up Token', {
+                position: toast.POSITION.TOP_RIGHT,
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                style: {
+                  background: '#ff5252', 
+                  color: '#ffffff', 
+                  fontSize: '16px',
+                },
+              });
+            } else {
+              toast.error('An Error Occurred, Please Try Again', {
+                position: toast.POSITION.TOP_RIGHT,
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                style: {
+                  background: '#ff5252', // Background color of the toast
+                  color: '#ffffff', // Text color of the toast
+                  fontSize: '16px', // Font size
+                },
+              })
+            }
+          })
+          
+        },
+        onError: (error) => {
+          console.log(error)
+        }
+      });
   return (
     <>
         <Navbar/>
         <div className='p-10'>
-            <form action="" className='w-full'>
-                <div className='grid grid-cols-1 md:flex my-1 bg-blue-100 p-2'>
-                    <label htmlFor="" className='basis-2/6'>Select The Term</label>
-                    <select name="" id="" className='basis-4/6 border-2 border-blue-400 p-2 '>
-                        <option value="">1<sup><small>st</small></sup> Term</option>
-                        <option value="">2<sup><small>nd</small></sup> Term</option>
-                        <option value="">3<sup><small>rd</small></sup> Term</option>
+            <form action="" onSubmit={formik.handleSubmit} className='w-full'>
+                <div className='my-1 bg-blue-100 p-2'>
+                    <label htmlFor="newTerm" className="text-gray-600">  The New Term</label>
+                    <select id="newTerm" name="newTerm" onChange={formik.handleChange} className="mt-1 p-2 block w-full border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500">
+                        <option value={1}>1<sup>st</sup> Term</option>    
+                        <option value={2}>2<sup>nd</sup> Term</option>    
+                        <option value={3}>3<sup>rd</sup> Term</option>    
                     </select>
+                {formik.touched.newTerm && formik.errors.newTerm ? (<p className="mt-2 text-sm text-red-600">{formik.errors.newTerm}</p>) : null}
                 </div>
                 <div className='grid grid-cols-1 md:flex my-1 bg-blue-100 p-2'>
-                    <label htmlFor="schoolFeeAmount" className='basis-2/6'>Set School Fee Amount</label>
-                    <input type="number" name="" id="" className='basis-4/6 border-2 border-blue-400 p-2 ' placeholder='Amount' />
+                    <label htmlFor="schoolFeesAmount" className='basis-2/6'>Set School Fee Amount</label>
+                    <input id="schoolFeesAmount" name="schoolFeesAmount" type="text" autoComplete="schoolFeesAmount" placeholder='Amount' value={formik.values.schoolFeesAmount} onChange={formik.handleChange} onBlur={formik.handleBlur} className="mt-1 p-2 block w-full border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"/>
+                    {formik.touched.schoolFeesAmount && formik.errors.schoolFeesAmount ? (<p className="mt-2 text-sm text-red-600">{formik.errors.schoolFeesAmount}</p>) : null}
                 </div>                
                 <div className='grid grid-cols-1 md:flex my-1 bg-blue-100 p-2'>
-                    <label htmlFor="schoolFeeAmount" className='basis-2/6'>Set Staff Salary</label>
-                    <input type="number" name="" id="" className='basis-4/6 border-2 border-blue-400 p-2 ' placeholder='Amount' />
-                </div>
+                    <label htmlFor="staffSalary" className='basis-2/6'>Set Staff Salary</label>
+                    <input id="staffSalary" name="staffSalary" type="text" autoComplete="staffSalary" placeholder='Amount' value={formik.values.staffSalary} onChange={formik.handleChange} onBlur={formik.handleBlur} className="mt-1 p-2 block w-full border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"/>
+                    {formik.touched.staffSalary && formik.errors.staffSalary ? (<p className="mt-2 text-sm text-red-600">{formik.errors.staffSalary}</p>) : null}
+                </div>                
                 <div className='grid grid-cols-1 md:flex my-1 bg-blue-100 p-2'>
-                    <label htmlFor="schoolFeeAmount" className='basis-2/6'>Input Your Admin Authorization Code</label>
-                    <input type="text" name="" id="" className='basis-4/6 border-2 border-blue-400 p-2 ' placeholder='Authorization Code' />
-                </div>
+                    <label htmlFor="authorizationCode" className='basis-2/6'>Input Your Admin Authorization Code</label>
+                    <input id="authorizationCode" name="authorizationCode" type="text" autoComplete="authorizationCode" placeholder='Amount' value={formik.values.authorizationCode} onChange={formik.handleChange} onBlur={formik.handleBlur} className="mt-1 p-2 block w-full border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"/>
+                    {formik.touched.authorizationCode && formik.errors.authorizationCode ? (<p className="mt-2 text-sm text-red-600">{formik.errors.authorizationCode}</p>) : null}
+                </div>                
                 <div className='my-1'>
-                    <input type="checkbox" name="" id="" className=' accent-blue-500' />
-                    <label htmlFor="">Are you sure you this term has ended, this will automatically generate your students information </label>
+                <label>
+                    <input type="checkbox" name="isChecked" checked={formik.values.isChecked} onChange={formik.handleChange}/> 
+                    Are you sure you this term has ended, this will automatically generate your students information
+                </label>
+                {formik.touched.isChecked && formik.errors.isChecked ? (<p className="mt-2 text-sm text-red-600">{formik.errors.isChecked}</p>) : null}
                 </div>             
-                <div className='flex'><button className='py-2 px-3 bg-blue-500 rounded-lg ms-auto'>Create Academic Term</button></div>
+                <div className='flex'><button type='submit' className='py-2 px-3 bg-blue-500 rounded-lg ms-auto'>Create Academic Term</button></div>
             </form>
         </div>
     </>
