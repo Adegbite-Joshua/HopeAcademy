423a3734fc0bf5e5b990ce804197ffa9aa6ec426
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2024-01-04 03:45:53 +0100

    Working on PWA feature

--- vite.config.js
+++ vite.config.js
@@ -7,51 +7,20 @@ export default defineConfig({
   server: {
     port: 2000,
     host: '0.0.0.0'
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
   }
 })
 
-
-// import { defineConfig } from 'vite';
-// import { VitePWA } from 'vite-plugin-pwa';
-// import WorkboxPlugin from 'workbox-vite-plugin';
-
-// export default defineConfig({
-//   plugins: [
-//     VitePWA(),
-//     WorkboxPlugin({
-//       // Workbox options go here
-//       // See: https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-webpack-plugin.GenerateSW#GenerateSW
-//       clientsClaim: true,
-//       skipWaiting: true,
-
-//       // Precaching options
-//       include: [/\.html$/, /\.js$/, /\.css$/, /\.png$/, /\.svg$/, /\.json$/],
-//       exclude: [/\.map$/, /^manifest.*\.js(?:on)?$/, /\.json$/],
-
-//       // Runtime caching options
-//       runtimeCaching: [
-//         {
-//           urlPattern: /\.(?:png|jpg|jpeg|webp|svg)$/,
-//           handler: 'CacheFirst',
-//           options: {
-//             cacheName: 'images-cache',
-//             expiration: {
-//               maxEntries: 50,
-//               maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-//             },
-//           },
-//         },
-//         {
-//           urlPattern: /\.(?:js|css)$/,
-//           handler: 'StaleWhileRevalidate',
-//           options: {
-//             cacheName: 'static-assets-cache',
-//           },
-//         },
-//       ],
-
-//       // Other options
-//       cleanupOutdatedCaches: true,
-//     }),
-//   ],
-// });
