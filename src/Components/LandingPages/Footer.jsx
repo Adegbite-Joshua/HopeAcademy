6d64ac7eef7d1ca1bb1315a86686fe6aab327b42
Adegbite Joshua Adeoye <adegbitejoshua07@gmail.com>
2024-10-23 14:45:21 +0100

    Workin gon the footer

--- src/Components/LandingPages/Footer.jsx
+++ src/Components/LandingPages/Footer.jsx
@@ -4,13 +4,26 @@ import { Link } from 'react-router-dom';
 const LandingPageFooter = () => {
   return (
     <>
-      <div className="bg-gray-100 py-8">
-        <div className="container mx-auto text-center">
-          <h3 className='text-2xl font-semibold mb-2'>Contact Us</h3>
-          <p className='text-sm mb-2'>89 Marsh Hill Road</p>
-          <p className='text-sm mb-2'>Orange, CT 06477</p>
-          <p className='text-sm mb-2'><small>Phone: +2347015886456</small></p>
-          <p className='text-sm mb-2'><small>adegbitejoshua07@gmail.com</small></p>
+      <div className='grid grid-cols-3 bg-gray-100 gap-2 py-2 px-24'>
+        <div className="p-8 ms-auto">
+          <img src="/school_logo.png" alt="School logo" className='w-full h-32' />
+          <h3 className="text-2xl">Every Child Deserves a Little HOPE...</h3>
+        </div>
+        <div className="p-8">
+            <h3 className='text-2xl font-semibold mb-2'>Overview</h3>
+            <Link className='text-blue-400' to='/'>Home</Link>
+            <Link className='text-blue-400' to='/student/signin'>Student Sign In</Link>
+            <Link className='text-blue-400' to='/staff/signin'>Staff Sign In</Link>
+            <Link className='text-blue-400' to='/admin/signin'>Admin Sign In</Link>
+            <Link className='text-blue-400' to='/about_us'>About Us</Link>
+            <Link className='text-blue-400' to='/contact_us'>Contact Us</Link>
+        </div>
+        <div className="p-8">
+            <h3 className='text-2xl font-semibold mb-2'>Contact Us</h3>
+            <p className='text-sm mb-2'>Ogbomoso, Oyo State.</p>
+            <p className='text-sm mb-2'>Nigeria</p>
+            <p className='text-sm mb-2'>Phone: <small><a href="tel:+2347015886456" className="text-blue-500 inline"> +2347015886456</a></small></p>
+            <p className='text-sm mb-2'>Email: <small><a className='inline text-blue-500' href="mailto:adegbitejoshua07@gmail.com"> adegbitejoshua07@gmail.com</a></small></p>
         </div>
       </div>
       <div className='bg-light py-2 px-5 flex flex-col md:flex-row items-center justify-between'>
